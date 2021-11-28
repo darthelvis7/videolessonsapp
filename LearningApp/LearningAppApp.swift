b2fecccbef68e623e8b0a4f1455ac406a322d948
Elvis Cuevas <cuevas.elvis@yahoo.com>
2021-11-28 12:24:15 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Elvis Cuevas on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
